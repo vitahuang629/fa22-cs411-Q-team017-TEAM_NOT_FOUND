b0e8036430c2fab0f7b5b325e5199e602033051e
Mengting AI <69413315+famous-blue-raincoat@users.noreply.github.com>
2022-11-11 23:23:23 -0600

    Delete frontend directory

--- frontend/Site1/box.js
+++
@@ -1,8 +0,0 @@
-function saveUn(event) {
-  var username = document.getElementById('un').value;
-  // alert(username);
-  var url = "http://localhost:8080/search?id=" + username;
-  // window.navigate(url);
-  window.location.replace(url);
-  event.preventDefault();
-  }
